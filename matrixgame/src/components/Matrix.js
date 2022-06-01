37e9b9e35c12d574855d8d8ac134b62d7151fcc4
iamali123 <58678609+iamali123@users.noreply.github.com>
2022-06-01 18:51:19 +0400

    matrix game

---
+++ matrixgame/src/components/Matrix.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './matrix.css';
+
+const Matrix = () => {
+
+
+
+  return (
+      <div className='matrixWrapper'>
+             <div className='box'>Box1</div>  
+             <div className='box'>Box2</div>  
+             <div className='box'>Box3</div>  
+             <div className='box'>Box4</div>  
+             <div className='box'>Box5</div>  
+             <div className='box'>Box6</div>  
+             <div className='box'>Box7</div>  
+             <div className='box'>Box8</div>  
+             <div className='box'>Box9</div>  
+             <div className='box'>Box10</div>  
+             <div className='box'>Box11</div>  
+             <div className='box'>Box12</div>  
+             <div className='box'>Box13</div>  
+             <div className='box'>Box14</div>  
+             <div className='box'>Box15</div>  
+             <div className='box'>Box16</div>  
+      </div>
+  )
+}
+
+export default Matrix
